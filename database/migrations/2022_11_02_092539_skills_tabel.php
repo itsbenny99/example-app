70e319fbe71653b6e7471b028153c8e2cad7e2c1
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2022-11-02 10:39:26 +0000

    Added Skill Migration

--- database/migrations/2022_11_02_092539_skills_tabel.php
+++ database/migrations/2022_11_02_092539_skills_tabel.php
@@ -13,9 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+         Schema::create('employee_skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('Role')->unique();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->interger('permissionid')->nullable();
+            $table->timestamps();
+        });
     }
-
+    
     /**
      * Reverse the migrations.
      *
@@ -23,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+           Schema::dropIfExists('employee_skills');
     }
 };
