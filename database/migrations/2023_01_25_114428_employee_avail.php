964ca1426d433826f063ca297a330df5a0f7174d
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-01-25 12:19:55 +0000

    Added Monday -> Sunday Start/End times.

--- database/migrations/2023_01_25_114428_employee_avail.php
+++ database/migrations/2023_01_25_114428_employee_avail.php
@@ -13,7 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('EmployeeAvail', function (Blueprint $table) {
+            $table->id();
+            $table->integer('MondayStart');
+            $table->integer('MondayEnd');
+            $table->integer('TuesdayStart');
+            $table->integer('TuesdayEnd');
+            $table->integer('WednesdayStart');
+            $table->integer('WednesdayEnd');
+            $table->integer('ThursdayStart');
+            $table->integer('ThursdayEnd');
+            $table->integer('FridayStart');
+            $table->integer('FridayEnd');
+            $table->integer('SaturdayStart');
+            $table->integer('SaturdayEnd');
+            $table->integer('SundayStart');
+            $table->integer('SundayEnd');
+            $table->integer('EmployeeId');
     }
 
     /**
