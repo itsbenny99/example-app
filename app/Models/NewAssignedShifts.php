e48d074243b31755cb439bd9afd1b474d4e14595
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-02-08 11:39:44 +0000

    Update NewAssignedShifts.php

--- app/Models/NewAssignedShifts.php
+++ app/Models/NewAssignedShifts.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\Model;
+
 class NewAssignedShifts extends Model
 {
     protected $table = "NewAssignedShifts";
