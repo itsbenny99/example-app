e845acdbef9ea331b5714c09e06bbba22bea5cca
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-02-14 13:54:21 +0000

    Update RequestedHoliday.php

--- app/Models/RequestedHoliday.php
+++ app/Models/RequestedHoliday.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RequestedHoliday extends Model
 {
-    use HasFactory;
+    $protected $table = "RequestedHoliday";
+    protected $fillable = [
+        'id',
+        'start_date',
+        'end_date',
+        'Reason',
+        'EmployeeId',
+        'status'
+        };
 }
