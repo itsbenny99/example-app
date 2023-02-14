93571ce35dfdc23e5817f0080a56dd73fbba9359
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-02-14 13:25:44 +0000

    Update 2023_02_13_121731_requested_holiday.php

--- database/migrations/2023_02_13_121731_requested_holiday.php
+++ database/migrations/2023_02_13_121731_requested_holiday.php
@@ -28,9 +28,6 @@ return new class extends Migration
      *
      * @return void
      */
-     public function down()
-    {
-        Schema::dropIfExists('RequestedHoliday');
-    }
+    
    
 };
