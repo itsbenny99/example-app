9cb62e4ac34927a89b6307787b27831449353df1
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-02-15 12:03:20 +0000

    Create Rholiday.php

---
+++ app/Models/Rholiday.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modles
+  
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+
+class Rholiday extends Model{
+}
+}
