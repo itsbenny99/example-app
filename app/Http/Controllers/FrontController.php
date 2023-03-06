a21d76eca7daa9b57d9127d3ce55d742d972dc33
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-06 11:03:17 +0000

    Update FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -24,6 +24,7 @@ class FrontController extends Controller
         }
         Log::error($dates);
         $shift = AssignedShifts::where('EmployeeId', '=', $user->id)->where('date', '=', $dates)->orderBy('date')->get();
+        Log::error($shift);
         if ($shift = null) { 
                $shift = null;
                 return view('dashboard')->with('shift', $shift);
