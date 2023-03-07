3dfbd835e5f2d48282fc6299391edcff741a1946
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-07 12:06:12 +0000

    Update create.blade.php

--- resources/views/User/Availability/create.blade.php
+++ resources/views/User/Availability/create.blade.php
@@ -11,7 +11,7 @@
                 <div class="overflow-x-auto relative">
                     <div class="w-full max-w-xs">
 
-                        {!! Form::open(['action' => ['App\Http\Controllers\EmployeeSkills@StoreEmployeeSkill']]) !!}
+                        {!! Form::open(['action' => ['App\Http\Controllers\Availability@PostAvailability']]) !!}
                         @csrf
                         
                          <div class ="mb-6">
@@ -44,5 +44,36 @@
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
                         
                         </div>
+                        <div class ="mb-6">
+                            {!! Form::label('end_time', 'End Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
+                            {!! Form::select('end_time', [ '',
+                              0 => '0000',
+                              1 => '0100',
+                              2 => '0200',
+                              3 => '0300',
+                              4 => '0400' ,
+                               5 => '0500',
+                              6 => '0600',
+                              7 => '0700',
+                              8 => '0800',
+                              9 => '0900' ,                            
+                              10 => '1000',
+                              11 => '1100',
+                              12 => '1200',
+                              13 => '1300',
+                              14 => '1400',
+                              15 => '1500',
+                              16 => '1600',
+                              17 => '1700',
+                              18 => '1800' ,
+                              19 => '1900',
+                              20 => '2000',
+                              21 => '2100',
+                              22 => '2200',
+                              23 => '2300' ,                   
+                              ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
+                        
+                        </div>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Save</button>
             {!! Form::close() !!}              
   </x-app-layout>
