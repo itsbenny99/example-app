e0771dd784e102911fca079743af516f266cd164
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-07 09:06:45 +0000

    Update RequestedEmployeeAvail.php

--- app/Models/RequestedEmployeeAvail.php
+++ app/Models/RequestedEmployeeAvail.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 
 class RequestedEmployeeAvail extends Model{
-    protected $table = "employeeavail";
+    protected $table = "EmployeeAvail";
     protected $fillable = [
         'id',
         'MondayEnd',
