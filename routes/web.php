14540814ee8ac7278cc0199c89bfe4d34f4589f3
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-07 09:11:54 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,31 @@ use App\Http\Controllers\CreateShift;
 use App\Http\Controllers\CalenderController;
 use App\Http\Controllers\FrontController;
 
+// Home Page(s)
 Route::get('/', [FrontController::class, 'home']);
 Route::get('/dashboard', [FrontController::class, 'home'])->name('dashboard');;
 
-
-Route::get('/user/addavail/{id}', [Availability::class, 'EditAvailability']);
-
-
-
+// Employee Availability 
+Route::get('/myavailability/{id}', [Availability::class, 'EditAvailability']);
+Route::get('/myavailability/new/{id}, [Availability::class, 'CreateAvailability']);
+Route::post('/myavailability/new/{id}, [Availability::class, 'PostAvailability']);
 
 //Employee Skills
 Route::get('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'EmployeeSkillsEdit']);
 Route::post('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'UpdateSkill']);
+
 Route::get('/admin/create/employeeskill', [EmployeeSkills::class, 'CreateEmployeeSkill']);
 Route::post('/admin/create/employeeskill', [EmployeeSkills::class, 'StoreEmployeeSkill']);
+
 Route::get('/admin/create/deleteemployeeskill/{id}', [EmployeeSkills::class, 'DeleteEmployeeSkill']);
 Route::get('/admin/userskills/{id}', [EmployeeSkills::class, 'UserSkills']);
 Route::get('/admin/employeeskills', [EmployeeSkills::class, 'EmployeeSkillsView']);
-
-
-
 
 Route::get('/admin/userlist/', [UserPermissions::class, 'UserViews']);
 Route::get('/admin/userlist/{id}', [UserPermissions::class, 'ShowUserUpdate']);
 Route::post('/admin/userlist/{id}', [UserPermissions::class, 'AddPermission']);
 
-
+/Shifts
 Route::get('/admin/createshift/', [CreateShift::class, 'ShowShift']);
 Route::post('/admin/createshift/', [CreateShift::class, 'StoreShift']);
 Route::get('/admin/showshift', [CreateShift::class, 'ShowStoredShifts']);
@@ -44,6 +43,5 @@ Route::get('/admin/AutoAssign', [CreateShift::class, 'AssignShift']);
 Route::get('calendar-event', [CalenderController::class, 'index']);
 Route::post('calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
 
-Route::get('/myavailability', [FrontController::class, 'ViewAvail']);
 
 require __DIR__.'/auth.php';
