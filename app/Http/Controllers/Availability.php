463a05a0c6249d0cf1abae743fc376414c505e55
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-07 09:04:32 +0000

    Update Availability.php

--- app/Http/Controllers/Availability.php
+++ app/Http/Controllers/Availability.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\RequestedEmployeeAvail;
 
 class Availability extends Controller
 {
     public function EditAvailability($id) {
         $users = User::find($id);
-
-        return view('User.Availability.edit')->with('users', $users);
+        Availability = RequestedEmployeeAvail::where('EmployeeId', '=', $id)->get();
+        return view('User.Availability.edit')->with('users', $users)->with('Availability', $Availability);
 
 
     }
