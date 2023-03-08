6a909e5acc0964145a77c20a1fc6aa77b09dcba9
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-08 11:57:39 +0000

    Update create.blade.php

--- resources/views/User/Availability/create.blade.php
+++ resources/views/User/Availability/create.blade.php
@@ -13,8 +13,8 @@
 
                         {!! Form::open(['action' => ['App\Http\Controllers\Availability@PostAvailability']]) !!}
                         @csrf
-                         <div style="width:800px;">
-                         <div style="width:300px; float:left;">
+                        <div style ="display: flex; justify-content: space-around;'>
+                        <div style =" border: 1px solid lightgray; background-color: red; width: 40%;">
                          <div class ="mb-6">
                             {!! Form::label('start_time', 'Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('name', [ '',
@@ -75,7 +75,8 @@
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
                   
                         </div>
-                        <div style="width:300px; float:right;"></div>
+                        </div>
+                        <div style=" border: 1px solid lightgray; background-color: green; width: 40%;">
                          <div class ="mb-6">
                             {!! Form::label('start_time', 'Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('name', [ '',
