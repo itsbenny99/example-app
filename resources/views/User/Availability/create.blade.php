a8bd4adcf42dc0460aeb1a08d1658a487f9a3755
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-08 11:54:58 +0000

    Update create.blade.php

--- resources/views/User/Availability/create.blade.php
+++ resources/views/User/Availability/create.blade.php
@@ -73,7 +73,7 @@
                               22 => '2200',
                               23 => '2300' ,                   
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
+                  
                         </div>
                         <div style="width:300px; float:right;"></div>
                          <div class ="mb-6">
