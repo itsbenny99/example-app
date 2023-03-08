ad86a17ce096df61b5094c2de7c98586d0511e0c
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-08 11:46:12 +0000

    Update edit.blade.php

--- resources/views/User/Availability/edit.blade.php
+++ resources/views/User/Availability/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <br>
-                <form action="/create/NewAvail">
+                <form action="/myavailability/new/">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Create New Availbility</button>
                 </form>
                 <div class="p-6 bg-white border-b border-gray-200"></div>
