56e5634b8e0fcba5e74ae00104c45c1effd97cf8
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-09 11:07:51 +0000

    Update create.blade.php

--- resources/views/User/Availability/create.blade.php
+++ resources/views/User/Availability/create.blade.php
@@ -17,7 +17,7 @@
                         <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
                          <div class ="mb-6">
                             {!! Form::label('monday_start_time', 'Monday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
-                            {!! Form::select('MondayStart', [ '',
+                            {!! Form::select('monday_start_time', [
                               0000 => '0000',
                               0100 => '0100',
                               0200 => '0200',
@@ -42,7 +42,7 @@
                               2100 => '2100',
                               2200 => '2200',
                               2300 => '2300' ,  
-                              -1 => 'Not Available,
+                              -1 => 'Not Available
                            
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
                         
