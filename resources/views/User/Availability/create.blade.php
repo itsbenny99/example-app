27d520e39877b2e8893ba76719943b613cd1acbd
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-14 11:51:05 +0000

    Update create.blade.php

--- resources/views/User/Availability/create.blade.php
+++ resources/views/User/Availability/create.blade.php
@@ -13,8 +13,7 @@
 
                         {!! Form::open(['action' => ['App\Http\Controllers\Availability@PostAvailability']]) !!}
                         @csrf
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                       
                          <div class ="mb-6">
                             <div class ="mb-6">
                             {!! Form::label('monday_start_time', 'Monday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
@@ -78,10 +77,7 @@
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
                   
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                      
                          <div class ="mb-6">
                             {!! Form::label('tuesday_start_time', 'Tuesday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('tuesday_start_time', [ '',
@@ -142,10 +138,7 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                        
                          <div class ="mb-6">
                             {!! Form::label('wednesday_start_time', 'Wednesday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('wednesday_start_time', [ '',
@@ -206,10 +199,8 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                    
+                       
                          <div class ="mb-6">
                             {!! Form::label('thursday_start_time', 'Thursday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('thursday_start_time', [ '',
@@ -270,10 +261,7 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                        
                          <div class ="mb-6">
                             {!! Form::label('friday_start_time', 'Friday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('friday_start_time', [ '',
@@ -334,10 +322,7 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                      
                          <div class ="mb-6">
                             {!! Form::label('saturday_start_time', 'Saturday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('saturday_start_time', [ '',
@@ -398,10 +383,7 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
-                        </div>
-                        </div>
-                        <div style ="display: flex; justify-content: space-around;'>
-                        <div style ="border: 1px solid lightgray; background-color: red; width: 70%;">
+                        
                          <div class ="mb-6">
                             {!! Form::label('sunday_start_time', 'Sunday Start Time', ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300']) !!}
                             {!! Form::select('sunday_start_time', [ '',
@@ -462,11 +444,9 @@
                               23 => '2300',     
                               25 => 'Not Available',
                               ], null,['class' => 'form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}
+                        
                         </div>
-                        </div>
-                      
-                        </div>
-                        <div style="clear: both;"></div>
+                        
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Save</button>
             {!! Form::close() !!}              
   </x-app-layout>
