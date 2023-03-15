290814f10a07455f85e0f09f41623b934491d229
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-15 11:57:25 +0000

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,11 @@
 
 
             <!-- Settings Dropdown -->
-            
+            <div style="    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 20px 50px;
+    box-shadow: 0 1px 8px #ddd;">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -96,7 +100,7 @@
 
                 </x-dropdown>
          
-
+</div>
 
 
             <!-- Hamburger -->
