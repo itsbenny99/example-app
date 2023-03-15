439efd87fdb4ee8652bad17f171f5cbb6eae4f25
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-15 11:06:15 +0000

    Update reviewavail.blade.php

--- resources/views/admin/reviewavail.blade.php
+++ resources/views/admin/reviewavail.blade.php
@@ -54,7 +54,7 @@
                                 </td>     
                                 <td class="py-4 px-6">
                                   <form action="/admin/accepted/employeeavail/{{$a->EmployeeId}}">
-                                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit">Accept</button>
+                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Accept</button>
                             </form>
                                 </td>  
                            </tr>
