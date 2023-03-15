4c97520c22148bd4ca9a1c03d69f456752fae9a4
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-15 11:41:07 +0000

    Update reviewavail.blade.php

--- resources/views/admin/reviewavail.blade.php
+++ resources/views/admin/reviewavail.blade.php
@@ -53,10 +53,10 @@
                                      {{$a->SundayStart}} -> {{$a->SundayEnd}}
                                 </td>     
                                 <td class="py-4 px-6">
-                                  <form action="/admin/accepted/employeeavail/{{$a->id}}/a/{{$a->EmployeeId}}">
+                                  <form action="/admin/accepted/employeeavail/{{$a->id}}/{{$a->EmployeeId}}/a">
                                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Accept</button>
                             </form>
-                             <form action="/admin/accepted/employeeavail/{{$a->id}}/r/{{$a->EmployeeId}}">
+                             <form action="/admin/accepted/employeeavail/{{$a->id}}/{{$a->EmployeeId}}/r">
                                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Reject</button>
                             </form>
                                 </td>  
