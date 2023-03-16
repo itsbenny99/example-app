141148d82d3221e559d2a2f609f4f1a137fadba8
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-16 13:15:58 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,6 +25,7 @@
                                                                 <th scope="row" class="py-4 px-6 font-medium text-gray-900                              whitespace-nowrap dark:text-white">
                                         {{$d}}
                                     </th>
+                                    @endforeach
                             @foreach($shift as $s)
                                
                                     <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
