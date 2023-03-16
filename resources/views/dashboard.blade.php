e1f92f396c337b02f3453c4e72af5f588558bc51
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-16 13:30:21 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,7 +42,8 @@
                                         {{$s->end_time}}
                                     </td>
                                 </tr>
-                           @else
+                           @endif
+                           @if ($s->date != $d)
                                  <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {{$d}}
                                  </th>
