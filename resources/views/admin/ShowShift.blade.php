8493e92e1220b976cbd63120befe68d70fd1889d
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-03-22 11:38:08 +0000

    Update ShowShift.blade.php

--- resources/views/admin/ShowShift.blade.php
+++ resources/views/admin/ShowShift.blade.php
@@ -10,7 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <br>
-                <form action="/admin/assign/shift">
+                <form action="/admin/AutoAssign">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Assign Shifts</button>
                 </form>
                 <div class="p-6 bg-white border-b border-gray-200"></div>
