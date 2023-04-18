a5f6ae5ddaeb18c791c710babf4e1b83945dc66b
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2023-04-18 09:02:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,39 +9,39 @@ use App\Http\Controllers\CalenderController;
 use App\Http\Controllers\FrontController;
 
 // Home Page(s)
-Route::get('/', [FrontController::class, 'home']);
-Route::get('/dashboard', [FrontController::class, 'home'])->name('dashboard');;
+Route::get('/', [FrontController::class, 'home'])->middleware('auth');;
+Route::get('/dashboard', [FrontController::class, 'home'])->name('dashboard')->middleware('auth');
 
 // Employee Availability 
-Route::get('/myavailability/', [Availability::class, 'EditAvailability']);
-Route::get('/myavailability/new', [Availability::class, 'CreateAvailability']);
-Route::post('/myavailability/new', [Availability::class, 'PostAvailability']);
+Route::get('/myavailability/', [Availability::class, 'EditAvailability'])->middleware('auth');
+Route::get('/myavailability/new', [Availability::class, 'CreateAvailability'])->middleware('auth');
+Route::post('/myavailability/new', [Availability::class, 'PostAvailability'])->middleware('auth');
 
-Route::get('/admin/accepted/employeeavail', [Availability::class, 'ReviewAvailability']);
-Route::get('/admin/accepted/employeeavail/{id}/{eid}/{a}' , [Availability::class, 'AcceptedAvailability']);
-Route::get('/admin/view/employeeavail', [Availability::class, 'ViewEmployee']);
+Route::get('/admin/accepted/employeeavail', [Availability::class, 'ReviewAvailability'])->middleware('auth');
+Route::get('/admin/accepted/employeeavail/{id}/{eid}/{a}' , [Availability::class, 'AcceptedAvailability'])->middleware('auth');
+Route::get('/admin/view/employeeavail', [Availability::class, 'ViewEmployee'])->middleware('auth');
 
 //Employee Skills
-Route::get('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'EmployeeSkillsEdit']);
-Route::post('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'UpdateSkill']);
+Route::get('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'EmployeeSkillsEdit'])->middleware('auth');
+Route::post('/admin/edit/employeeskill/{id}', [EmployeeSkills::class, 'UpdateSkill'])->middleware('auth');
 
-Route::get('/admin/create/employeeskill', [EmployeeSkills::class, 'CreateEmployeeSkill']);
-Route::post('/admin/create/employeeskill', [EmployeeSkills::class, 'StoreEmployeeSkill']);
+Route::get('/admin/create/employeeskill', [EmployeeSkills::class, 'CreateEmployeeSkill'])->middleware('auth');
+Route::post('/admin/create/employeeskill', [EmployeeSkills::class, 'StoreEmployeeSkill'])->middleware('auth');
 
-Route::get('/admin/create/deleteemployeeskill/{id}', [EmployeeSkills::class, 'DeleteEmployeeSkill']);
-Route::get('/admin/userskills/{id}', [EmployeeSkills::class, 'UserSkills']);
-Route::get('/admin/employeeskills', [EmployeeSkills::class, 'EmployeeSkillsView']);
+Route::get('/admin/create/deleteemployeeskill/{id}', [EmployeeSkills::class, 'DeleteEmployeeSkill'])->middleware('auth');
+Route::get('/admin/userskills/{id}', [EmployeeSkills::class, 'UserSkills'])->middleware('auth');
+Route::get('/admin/employeeskills', [EmployeeSkills::class, 'EmployeeSkillsView'])->middleware('auth');
 
-Route::get('/admin/userlist/', [UserPermissions::class, 'UserViews']);
-Route::get('/admin/userlist/{id}', [UserPermissions::class, 'ShowUserUpdate']);
-Route::post('/admin/userlist/{id}', [UserPermissions::class, 'AddPermission']);
+Route::get('/admin/userlist/', [UserPermissions::class, 'UserViews'])->middleware('auth');
+Route::get('/admin/userlist/{id}', [UserPermissions::class, 'ShowUserUpdate'])->middleware('auth');
+Route::post('/admin/userlist/{id}', [UserPermissions::class, 'AddPermission'])->middleware('auth');
 
 //Shifts
-Route::get('/admin/createshift/', [CreateShift::class, 'ShowShift']);
-Route::post('/admin/createshift/', [CreateShift::class, 'StoreShift']);
-Route::get('/admin/showshift', [CreateShift::class, 'ShowStoredShifts']);
-Route::get('/admin/shift/allocated/', [CreateShift::class, 'ShowAllocatedShifts']);
-Route::get('/admin/AutoAssign', [CreateShift::class, 'AssignShift']);
+Route::get('/admin/createshift/', [CreateShift::class, 'ShowShift'])->middleware('auth');
+Route::post('/admin/createshift/', [CreateShift::class, 'StoreShift'])->middleware('auth');
+Route::get('/admin/showshift', [CreateShift::class, 'ShowStoredShifts'])->middleware('auth');
+Route::get('/admin/shift/allocated/', [CreateShift::class, 'ShowAllocatedShifts'])->middleware('auth');
+Route::get('/admin/AutoAssign', [CreateShift::class, 'AssignShift'])->middleware('auth');
 
 
 Route::get('calendar-event', [CalenderController::class, 'index']);
